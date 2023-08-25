8d53a18cc81dbb94360b249a56cf44e2502ec280
Javier <javiersoap@gmail.com>
2023-08-25 14:44:52 +0200

    NextSu update

--- NextSu.java
+++ NextSu.java
@@ -15,21 +15,45 @@ public class NextSu {
 
     private int next(int i) {
         while(i != id[i]) {
-            id[i] = id[id[i]];
+            id[i] = id[id[i]] >= 0 ? id[id[i]] : id[i];
             i = id[i];
         }
-        return i;
+        return i < 0 ? -i : i;
     }
+
 
     public int find(int i) {
         return id[next(i)];
     }
 
     public void remove(int i) {
-        if(find(i) == i) {
-            return;
+        int p = next(i);
+        int q = next(i+1);
+        if (p == q) return;
+
+        if(size[i] < size[q]) {
+            id[i] = q;
+            size[q] += size[i];
+        } else {
+            id[q] = i;
+            size[i] += size[q];
         }
-        int j = next(i+1);
+        return;
+    }
+
+    public static void main(String[] args) {
+        NextSu test = new NextSu(10);
+        StdOut.println(test.find(1));
+        StdOut.println(test.find(5));
+        test.remove(1);
+        StdOut.println("Removed 1");
+        StdOut.println("find 1: " + test.find(1));
+        test.remove(5);
+        StdOut.println(test.find(5));
+        test.remove(4);
+        test.remove(3);
+        test.remove(2);
+        StdOut.println(test.find(1));
         
     }
 }
