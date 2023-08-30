a19437e8dc8d5a152018f20ec3d92c53d18df6c9
Javier <95035189+Pankeking@users.noreply.github.com>
2023-08-30 19:19:38 +0200

    Update ThreeSum.java

--- ThreeSum.java
+++ ThreeSum.java
@@ -1,8 +1,16 @@
+import java.utils.Arrays;
 
 public class ThreeSum {
     
+    private int[] numbers;
+    private int[] result;
+    
     public ThreeSum() {
         // std input txt file into array
+        while (StdIn.isEmpty) {
+            numbers 
+        }
+        Arrays.sort(numbers);
         // sort array?
         // loop 2 numbers as case  O(n^2)
         // binary search their negative sum
