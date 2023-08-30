312fe854ea8264fd2ed866c8a8aed7fe80d16321
Javier <95035189+Pankeking@users.noreply.github.com>
2023-08-30 15:43:56 +0200

    ThreeSum pseudocode init

--- ThreeSum.java
+++ ThreeSum.java
@@ -2,7 +2,13 @@
 public class ThreeSum {
     
     public ThreeSum() {
-        
+        // std input txt file into array
+        // sort array?
+        // loop 2 numbers as case  O(n^2)
+        // binary search their negative sum
+        // append numbers, i,j,k together to result[]
+        // result[] size
+        // check repeated trios?
     } 
 
     public static void main(String[] args) {
