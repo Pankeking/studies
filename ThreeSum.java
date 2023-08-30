6823196a69006ae86a974d5a55174173a2fccea6
Javier <95035189+Pankeking@users.noreply.github.com>
2023-08-30 19:41:03 +0200

    Update ThreeSum.java

--- ThreeSum.java
+++ ThreeSum.java
@@ -1,25 +1,44 @@
-import java.utils.Arrays;
+import java.util.Arrays;
+import edu.princeton.cs.algs4.StdIn;
 
 public class ThreeSum {
     
     private int[] numbers;
     private int[] result;
     
-    public ThreeSum() {
-        // std input txt file into array
-        while (StdIn.isEmpty) {
-            numbers 
+    public ThreeSum(int n) {
+        numbers = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int number = StdIn.readInt();
+            numbers[i] = number;
         }
+
         Arrays.sort(numbers);
-        // sort array?
         // loop 2 numbers as case  O(n^2)
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j++) {
+                int k = binarySearch(numbers, -(numbers[i] + numbers[j]));
+            }
+        }
         // binary search their negative sum
         // append numbers, i,j,k together to result[]
         // result[] size
         // check repeated trios?
     } 
 
+    private int binarySearch(int[] numbersArray, int doubleSum) {
+        int newIndex = numbersArray.length / 2;
+
+        if (numbersArray[newIndex] == doubleSum) return newIndex;
+        else if (numbersArray[newIndex] > doubleSum) {
+            // cut array in half?? java??
+            return binarySearch(
+        }
+    }
+
     public static void main(String[] args) {
-        
+        int n = Integer.parseInt(args[0]);
+        ThreeSum threeSum = new ThreeSum(n);
     }
 }
