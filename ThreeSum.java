3dc596d809fbe4c8117c7b5a39a282c73ffc913f
Javier <95035189+Pankeking@users.noreply.github.com>
2023-08-31 08:51:25 +0200

    Update ThreeSum.java

--- ThreeSum.java
+++ ThreeSum.java
@@ -4,10 +4,12 @@ import edu.princeton.cs.algs4.StdIn;
 public class ThreeSum {
     
     private int[] numbers;
-    private int[] result;
+    private int[][] result;
+    private int[] trie;
     
     public ThreeSum(int n) {
-        numbers = new int[n];
+        numbers = new int[n][3];
+        int resultIndex = 0;
 
         for (int i = 0; i < n; i++) {
             int number = StdIn.readInt();
@@ -17,24 +19,31 @@ public class ThreeSum {
         Arrays.sort(numbers);
         // loop 2 numbers as case  O(n^2)
         for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j++) {
+            for (int j = i + 1; j < n; j++) {
                 int k = binarySearch(numbers, -(numbers[i] + numbers[j]));
+                if (k != -1 && k > j) {
+                    result[resultIndex] = new int[]{numbers[i], numbers[j], numbers[k]};
+                    resultIndex++;
+                }
             }
         }
-        // binary search their negative sum
-        // append numbers, i,j,k together to result[]
         // result[] size
         // check repeated trios?
     } 
 
-    private int binarySearch(int[] numbersArray, int doubleSum) {
-        int newIndex = numbersArray.length / 2;
-
-        if (numbersArray[newIndex] == doubleSum) return newIndex;
-        else if (numbersArray[newIndex] > doubleSum) {
-            // cut array in half?? java??
-            return binarySearch(
+    private int binarySearch(int[] a, int key) {
+        int left = 0;
+        int right = a.length;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (a[mid] == key) return mid;
+            else if (a[mid] > key) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
         }
+        return -1;
     }
 
     public static void main(String[] args) {
