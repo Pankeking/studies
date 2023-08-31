7cf6e2472f6fb09cf3ff8fc138e7d430c1a614fe
Javier <javiersoap@gmail.com>
2023-08-31 14:16:00 +0200

    same commit as before, non saved files

--- 3Sumtxt.py
+++ 3Sumtxt.py
@@ -2,11 +2,12 @@ import random
 
 file_path = "3Sum.txt"
 
-lower_bound = -1000
-upper_bound = 1000
-num_integers = 200
+lower_bound = -100000
+upper_bound = 100000
+num_integers = 10000
 
 randomNumbers = [random.randint(lower_bound, upper_bound) for _ in range(num_integers)]
 
 with open(file_path, 'w') as file:
+    file.write(f"{num_integers}\n\n")
     file.write("\n".join(map(str, randomNumbers)))
