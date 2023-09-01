41bcf468cf604fa4dfeacfb995d31c7f17d64cc2
Javier <95035189+Pankeking@users.noreply.github.com>
2023-09-01 14:29:11 +0200

    Update PitonicSearch.java

--- PitonicSearch.java
+++ PitonicSearch.java
@@ -3,25 +3,32 @@ import edu.princeton.cs.algs4.StdIn;
 public class PitonicSearch {
     
     private int[] pitonicArray;
+    private boolean result;
 
-    public PitonicSearch(int sampleSize) {
+    public PitonicSearch(int sampleSize, int target) {
         for (int i = 0; i < sampleSize; i++) {
             
             int number = StdIn.readInt();
             pitonicArray[i] = number;
         }
+        result = binarySearch(pitonicArray, target);
     }
 
-    private void binarySearch(int i, int j, int q) {
-        // middle? 
+    private boolean binarySearch(int[] pitonicArray, int target) {
+        int mid = (pitonicArray.length - 1) / 2;
+        int left = 0;
+        int right = pitonicArray.length - 1;
         // search left
+        if (pitonicArray[mid] == target) return True;
         // search right
+       
         // repeat 
     }
 
     public static void main(String[] args) {
         int sampleSize = StdIn.readInt();
-
-        PitonicSearch test = new PitonicSearch(sampleSize);
+        int target = 77; // test
+        PitonicSearch test = new PitonicSearch(sampleSize, target);
+        StdOut.println(test.result);
     }
 }
