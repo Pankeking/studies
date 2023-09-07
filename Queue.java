41a6614c25de0e3b9bebc831e55a150388e2c21e
Javier <javiersoap@gmail.com>
2023-09-07 15:17:08 +0200

    Queues and stacks

---
+++ Queue.java
@@ -0,0 +1,22 @@
+public class Queue {
+    
+    private Node first = null;
+    private Node last = null;
+
+    private class Node {
+        String item;
+        Node next;
+    }
+
+    public void enQueue(String item) {
+        
+    }
+
+    public String deQueue() {
+
+    }
+
+    public boolean isEmpty() {
+
+    }
+}
