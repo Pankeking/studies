d58fa57cb026912e87b0b60bc71309c3809556f9
Javier <javiersoap@gmail.com>
2023-09-10 12:41:52 +0200

    deque boilerplate

---
+++ Deque.java
@@ -0,0 +1,41 @@
+public class Deque<Item> implements Iterable<Item> {
+
+    // Construct an empty deque
+    public Deque() {
+
+    }
+
+    // is the deQueue empty?
+    public boolean isEmpty() {
+
+    }
+    // return the number of items in the deque
+    public int size() {
+
+    }
+    // add item to the front
+    public void addFirst(Item item) {
+
+    }
+    // add item to the back
+    public void addLast(Item item) {
+
+    }
+    // remove and return item from the front
+    public Item removeFirst() {
+
+    }
+    // remove and return item from the back
+    public Item removeLast() {
+
+    }
+    // return an iterator over items from front to back
+    public Iterator<Item> iterator() {
+
+    }
+    
+    public static void main(String[] args) {
+
+    }
+
+}
