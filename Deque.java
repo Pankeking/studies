3505e35fbea248c1bb72f0631a792a9fcb530e2a
Javier <javiersoap@gmail.com>
2023-09-10 13:07:42 +0200

    deque construct and sizer

--- Deque.java
+++ Deque.java
@@ -11,35 +11,38 @@ public class Deque<Item> implements Iterable<Item> {
         Node down;
     }
     
-
     // Construct an empty deque
     public Deque() {
-
+        Node front = new Node();
+        Node back = new Node();
     }
 
     // is the deQueue empty?
     public boolean isEmpty() {
         return dequeSize == 0;
     }
+
     // return the number of items in the deque
     public int size() {
         return dequeSize;
     }
+
     // add item to the front
     public void addFirst(Item item) {
-
+        dequeSize++;
     }
+
     // add item to the back
     public void addLast(Item item) {
-
+        dequeSize++;
     }
     // remove and return item from the front
     public Item removeFirst() {
-
+        dequeSize--;
     }
     // remove and return item from the back
     public Item removeLast() {
-
+        dequeSize--;
     }
     // return an iterator over items from front to back
     public Iterator<Item> iterator() {
