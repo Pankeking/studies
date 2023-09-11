262415bd00c192aa13d61a6fe403aa3e21585aba
Javier <javiersoap@gmail.com>
2023-09-11 10:48:40 +0200

    boiler randomqueue

---
+++ RandomizedQueue.java
@@ -0,0 +1,34 @@
+import java.util.Iterator;
+import edu.princeton.cs.algs4.StdOut;
+
+public class RandomizedQueue<Item> implements Iterable<Item> {
+
+    public RandomizedQueue() {
+
+    }
+
+    // is the RandomizedQueue empty?
+    public boolean isEmpty() {
+
+    }
+
+    // return the numbers of items on the randomized queue
+    public int size() {
+
+    }
+
+    // add the item
+    public void enQueue(Item item) {
+
+    }
+
+    // remove and return random item
+    public Item deQueue() {
+
+    }
+
+    // return a random item without removing it
+    sample
+    Iterator<Item> iterator()
+    main
+}
