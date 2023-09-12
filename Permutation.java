57e14ec1aaacb6d1d15f258ea5f40168fdaa06b8
Javier <javiersoap@gmail.com>
2023-09-12 21:30:37 +0200

    permutation test?

--- Permutation.java
+++ Permutation.java
@@ -1,5 +1,17 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
 public class Permutation {
     public static void main(String[] args) {
-        
+        int k = Integer.parseInt(args[0]);
+        String[] strings = new String[k];
+        for (int i = 0; i < k; i++) {
+            strings[i] = StdIn.readString();
+        }
+        StdRandom.shuffle(strings);
+        for (String s : strings) {
+            StdOut.println(s);
+        }
     }
 }
