49674c1f3faf27c50ff7e55ae985299b22441661
Javier <javiersoap@gmail.com>
2023-09-18 23:28:34 +0200

    boiler

--- SelectionSort.java
+++ SelectionSort.java
@@ -3,9 +3,12 @@ import edu.princeton.cs.algs4.StdIn;
 public class SelectionSort {
     public static void main(String[] args) {
 
-        int sampleSize = Integer.parseInt(args[0]);
-        while(!StdIn.isEmpty()) {
-            
+        int[] numbers = StdIn.readAllInts();
+        int min = numbers[0];
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[j] < numbers[i]) min = numbers[j];
+            }
         }
     }
 }
