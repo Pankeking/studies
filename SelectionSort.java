2ddf2401dfc7577e3ba96f1a55c217fb71e33e88
Javier <javiersoap@gmail.com>
2023-09-18 23:06:22 +0200

    selection sort

--- SelectionSort.java
+++ SelectionSort.java
@@ -3,7 +3,7 @@ import edu.princeton.cs.algs4.StdIn;
 public class SelectionSort {
     public static void main(String[] args) {
 
-        int sampleSize = Integer.parseInt(StdIn.readString());
+        int sampleSize = Integer.parseInt(args[0]);
         while(!StdIn.isEmpty()) {
             
         }
