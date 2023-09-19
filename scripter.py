a10e62b8e1e0ac5983764e81e807a5d9b7a35453
Javier <javiersoap@gmail.com>
2023-09-19 18:53:31 +0200

    scripter better way

--- scripter.py
+++ scripter.py
@@ -5,15 +5,15 @@ if len(sys.argv) != 3:
     print("Usage: scripter.py <amount> <shuffle || sorted>")
     sys.exit(1)
     
-amount = int(sys.argv[1])
-sorting = sys.argv[2]
+input_size = int(sys.argv[1])
+init_state = sys.argv[2]
 
-input_data = f"{amount} < {sorting}_{amount}_int.txt"
+input_data = f"{input_size} < {init_state}_{input_size}_int.txt"
 
-command = f'''python numCreator.py {amount} {sorting} 
-            && java-algs4 SelectionSort {input_data} 
-            && java-algs4 Insertion {input_data} 
-            && java-algs4 ShellSort {input_data}
-            && java-algs4 MergeSort {input_data}
-            '''
+selection = f"&& java-algs SelectionSort {input_data}"
+insertion = f"&& java-algs4 Insertion {input_data}"
+shell = f"&& java-algs4 ShellSort {input_data}"
+merge = f"&& java-algs4 MergeSort {input_data}"
+
+command = f"python numCreator.py {input_size} {init_state} {shell} {merge}"
 os.system(command)
