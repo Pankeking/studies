140994cad1bc25601444ec4a0c109dc9dba82b03
Javier <javiersoap@gmail.com>
2023-09-20 00:48:05 +0200

    scripter updated to use python merge

--- scripter.py
+++ scripter.py
@@ -8,13 +8,15 @@ if len(sys.argv) != 3:
 input_size = int(sys.argv[1])
 init_state = sys.argv[2]
 
-input_data = f"{input_size} < {init_state}_{input_size}_int.txt"
+input_file = f"{init_state}_{input_size}_int.txt"
 
-selection = f"&& java-algs4 SelectionSort {input_data}"
-insertion = f"&& java-algs4 Insertion {input_data}"
-shell = f"&& java-algs4 ShellSort {input_data}"
-merge = f"&& java-algs4 MergeSort {input_data}"
-b_up_merge = f"&& java-algs4 BottomUpMerge {input_data}"
+selection = f"&& java-algs4 SelectionSort {input_size} < {input_file}"
+insertion = f"&& java-algs4 Insertion {input_size} < {input_file}"
+shell = f"&& java-algs4 ShellSort {input_size} < {input_file}"
+merge = f"&& java-algs4 MergeSort {input_size} < {input_file}"
+b_up_merge = f"&& java-algs4 BottomUpMerge {input_size} < {input_file}"
+
+python_merge = f"&& python merge_sort.py {input_file}"
 
 command = f"python numCreator.py {input_size} {init_state} {shell} {merge} + {b_up_merge}"
 os.system(command)
