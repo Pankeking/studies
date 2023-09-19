989273d77fda56b76b3034783b750a3e38347512
Javier <javiersoap@gmail.com>
2023-09-19 11:57:42 +0200

    shuffled sorted .txt

--- numCreator.py
+++ numCreator.py
@@ -1,16 +1,19 @@
 import random
 import sys
 
-if len(sys.argv) != 2:
-    print("Usage: python numCreator.py <number>")
+
+if len(sys.argv) != 3 or sys.argv[2] not in ["shuffled","sorted"]:
+    print("Usage: python numCreator.py <number> <shuffled || sorted>")
     sys.exit(1)
 
-
+method = sys.argv[2]
 number_qty = int(sys.argv[1])
-file_path = f"{number_qty}_int.txt"
 
 numbers = (list(range(number_qty)))
-random.shuffle(numbers)
+if method == "shuffled":
+    random.shuffle(numbers)
+file_path = f"{method}_{number_qty}_int.txt"
+    
 
 with open(file_path, 'w') as file:
     # file.write(f"{number_qty} random integers\n\n")
--- scripter.py
+++ scripter.py
@@ -1,10 +1,11 @@
 import os
 import sys
 
-if len(sys.argv) != 2:
-    print("Usage: scripter.py <amount>")
+if len(sys.argv) != 3:
+    print("Usage: scripter.py <amount> <shuffle-method>")
     sys.exit(1)
     
 amount = int(sys.argv[1])
-command = f"python numCreator.py {amount} && java-algs4 SelectionSort {amount} < {amount}_int.txt && java-algs4 Insertion {amount} < {amount}_int.txt"
+method = sys.argv[2]
+command = f"python numCreator.py {amount} {method} && java-algs4 SelectionSort {amount} < {method}_{amount}_int.txt && java-algs4 Insertion {amount} < {method}_{amount}_int.txt"
 os.system(command)
