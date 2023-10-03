a8022cf5c0495827c86699e6d5305b24ed4024da
Javier <javiersoap@gmail.com>
2023-10-03 14:05:19 +0200

    puzzles test

--- Board.java
+++ Board.java
@@ -1,18 +1,39 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
 public class Board {
 
+    private int N;
+    private int[][] board;
     // create a board from an n-by-n array of tiles,
     // where tiles[row][col] = tile at (row, col)
     public Board(int[][] tiles) {
-        return;
+        
+        for (int i = 0; i < tiles.length; i++) {
+            if (tiles[i].length != tiles.length) {
+                throw new IllegalArgumentException("Not n-by-n dimensions");
+            }
+        }
+        N = tiles.length;
+        board = tiles;
     }
     // string representation of this board
     public String toString() {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        sb.append(N + "\n");
+        for (int i = 0; i < N; i++) {
+            sb.append(" ");
+            for (int j = 0; j < N; j++) {
+                sb.append(board[i][j] + " ");
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
     }
 
     // board dimension n
     public int dimension() {
-        return -1;
+        return N;
     }
 
     // number of tiles out of place
@@ -47,7 +68,20 @@ public class Board {
 
     // unit testing (not graded)
     public static void main(String[] args) {
-        return;
+        
+        In in = new In(args[0]);
+        int n = in.readInt();
+        int [][] tiles = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                tiles[i][j] = in.readInt();
+            }
+        }
+                
+        Board board = new Board(tiles);
+        String sBoard = board.toString();
+        StdOut.println(sBoard);
+        
     }
 
 }
