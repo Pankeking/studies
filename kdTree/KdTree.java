3eeba25494b69011700263f0fba4c9f32f018bda
Javier <javiersoap@gmail.com>
2024-02-02 17:35:59 +0100

    added vertical alignment comparator, added rect insertion

--- kdTree/KdTree.java
+++ kdTree/KdTree.java
@@ -42,11 +42,11 @@ public class KdTree {
   public void insert(Point2D p) {
     if (p == null) throw new IllegalArgumentException();
     RectHV rect = new RectHV(0, 0, 1, 1);
-    Node root = insert(p, -1, this.root, rect);
+    Node root = insert(p, true, this.root, rect);
     this.root = root;
   }
 
-  private Node insert(Point2D p, int axis, Node root, RectHV rect) { // axis -1 X  // axis 1 Y
+  private Node insert(Point2D p, boolean vertical, Node root, RectHV rect) { // axis X true  // axis 1 Y
     this.size++;
     if (root == null) {
       Node node = new Node(p);
@@ -54,25 +54,36 @@ public class KdTree {
       return node;
     }
     double cmp;
-    if (axis < 0) cmp = root.p.x() - p.x();
+    // Positive compare  for lower point -> left/bottom
+    // Negative && equal for higher point -> right/top
+    if (vertical) cmp = root.p.x() - p.x(); 
     else          cmp = root.p.y() - p.y();
 
-    axis = axis > 0 ? -1 : 1;
-
     RectHV nextRect = null;
-    if (axis < 0) {
+    if (vertical) {
       nextRect = new RectHV(rect.xmin(), rect.ymin(), root.p.x(), rect.ymax());
     } else {
       nextRect = new RectHV(rect.xmin(), rect.ymin(), rect.xmax(), root.p.y());
     }
 
-   
-    if (cmp > 0)  {
-      Node node = insert(p, axis, root.lb, nextRect);
+    if (cmp > 0 && vertical)  {
+      nextRect = new RectHV(rect.xmin(), rect.ymin(), root.p.x(), rect.ymax());
+      Node node = insert(p, !vertical, root.lb, nextRect);
       root.lb = node;
     } 
-    else if (cmp <= 0) {
-      Node node = insert(p, axis, root.rt, nextRect);
+    else if (cmp > 0 && !vertical)  {
+      nextRect = new RectHV(rect.xmin(), root.p.y(), rect.xmax(), rect.ymax());
+      Node node = insert(p, !vertical, root.lb, nextRect);
+      root.lb = node;
+    } 
+    else if (cmp <= 0 && vertical) {
+      nextRect = new RectHV(root.p.x(), rect.ymin(), rect.xmax(), rect.ymax());
+      Node node = insert(p, !vertical, root.rt, nextRect);
+      root.rt = node;
+    }
+    else if (cmp <= 0 && !vertical) {
+      nextRect = new RectHV(rect.xmin(), rect.ymin(), rect.xmax(), root.p.y());
+      Node node = insert(p, !vertical, root.rt, nextRect);
       root.rt = node;
     }
     return root;
@@ -82,18 +93,17 @@ public class KdTree {
     if (p == null) throw new IllegalArgumentException();
     if (this.root == null) return false;
     Node current = this.root;
-    int axis = -1;
+    boolean vertical = true;
     double cmp;
     while (current != null) {
       if (current.p.equals(p)) return true;
 
-      if (axis < 0) {
+      if (vertical) {
         cmp = current.p.x() - p.x();
-        axis = 1;
       } else {
         cmp = current.p.y() - p.y();
-        axis = -1;
       }
+      vertical = !vertical;
 
       if (cmp > 0) {
         current = current.lb;
@@ -107,7 +117,9 @@ public class KdTree {
 
   public void draw() {
     if (root != null) {
-      draw(root, true);
+      StdDraw.setXscale(0, 1);
+      StdDraw.setYscale(0, 1);
+      draw(root, false);
     }
     return;
   }
@@ -115,10 +127,29 @@ public class KdTree {
     if (node == null) {
       return;
     }
+    // Draw Point
+    StdDraw.setPenColor(StdDraw.BLACK);
+    StdDraw.setPenRadius(0.01);
+    node.p.draw();
+
+    // Draw Line
+    if (vertical) {
+      StdDraw.setPenColor(StdDraw.RED);
+      StdDraw.setPenRadius();
+      StdDraw.line(node.p.x(), node.rect.ymin(), node.p.x(), node.rect.ymax());
+    } else {
+      StdDraw.setPenColor(StdDraw.BLUE);
+      StdDraw.setPenRadius();
+      StdDraw.line(node.rect.xmin(), node.p.y(), node.rect.xmax(), node.p.y());
+    }
+
+    // Recursively draw left-bottom and right-top subtrees
+    draw(node.lb, !vertical);
+    draw(node.rt, !vertical);
   }
 
   public Iterable<Point2D> range(RectHV rect) {
-    Queue<Point2D> points = new Queue<Point2D>;
+    Queue<Point2D> points = new Queue<Point2D>();
     range(this.root, rect, points);
     return points;
   }
@@ -134,20 +165,20 @@ public class KdTree {
 
 
   public static void main(String[] args) {
-    Point2D queryPoint = new Point2D(0.5, 0.7);
-    KdTree tree = new KdTree();
-    tree.insert(queryPoint);
+    // Point2D queryPoint = new Point2D(0.5, 0.7);
+    // KdTree tree = new KdTree();
+    // tree.insert(queryPoint);
 
-    for (int i = 0; i < 12800; i++) {
-        double x = StdRandom.uniformDouble(0.0, 1.0);
-        double y = StdRandom.uniformDouble(0.0, 1.0);
-        Point2D point = new Point2D(x, y);
-        tree.insert(point);
-        StdOut.printf(" Found: %b\n", tree.contains(point));
-    }
+    // for (int i = 0; i < 12800; i++) {
+    //     double x = StdRandom.uniformDouble(0.0, 1.0);
+    //     double y = StdRandom.uniformDouble(0.0, 1.0);
+    //     Point2D point = new Point2D(x, y);
+    //     tree.insert(point);
+    //     StdOut.printf(" Found: %b\n", tree.contains(point));
+    // }
 
-    StdOut.println(tree.contains(new Point2D(0.5, 0.7)));
-    StdOut.println(tree.contains(new Point2D(0.3, 0.3)));
-    return;
+    // StdOut.println(tree.contains(new Point2D(0.5, 0.7)));
+    // StdOut.println(tree.contains(new Point2D(0.3, 0.3)));
+    // return;
   }
 }
--- kdTree/KdTreeVisualizer.java
+++ kdTree/KdTreeVisualizer.java
@@ -23,10 +23,8 @@ public class KdTreeVisualizer {
             if (StdDraw.isMousePressed()) {
                 double x = StdDraw.mouseX();
                 double y = StdDraw.mouseY();
-                StdOut.printf("%8.6f %8.6f\n", x, y);
                 Point2D p = new Point2D(x, y);
                 if (rect.contains(p)) {
-                    StdOut.printf("%8.6f %8.6f\n", x, y);
                     kdtree.insert(p);
                     StdDraw.clear();
                     kdtree.draw();
